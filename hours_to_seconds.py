b1535d9f9a07565fa6aa3e89d09c17e5f270f2d2
isaacmario <isaac.mwangale7@gmail.com>
2024-10-11 17:44:52 +0300

    Add files via upload

---
+++ hours_to_seconds.py
@@ -0,0 +1,5 @@
+#converting hours to seconds 
+hours = 2
+
+seconds = hours * 3600
+print("2 hours is ",seconds, "seconds")
---
+++ rectangle_area.py
@@ -0,0 +1,6 @@
+#Finding area of the circle
+length = 10
+width = 5
+
+Area = (length * width)
+print(Area)
